f9035d34c5b8fbcd75667487db269e7a2d932d4f
Ameer Hamza <61797568+Hamza50537@users.noreply.github.com>
2023-07-19 18:04:27 +0500

    Update app.py for to run infinite times

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import datetime;
 import socket;
- 
-# current_Time stores current time
-current_Time = datetime.datetime.now()
-print("current time:-", current_Time)
- 
-# time_Stamp store timestamp of current time
-time_Stamp = current_Time.timestamp()
-print("timestamp:-", time_Stamp)
 
-host_Name=socket.gethostname()
-print("current host name is:-", host_Name )
+while True:
+    # current_Time stores current time
+    current_Time = datetime.datetime.now()
+    print("current time:-", current_Time)
+ 
+    # time_Stamp store timestamp of current time
+    time_Stamp = current_Time.timestamp()
+    print("timestamp:-", time_Stamp)
+
+    host_Name=socket.gethostname()
+    print("current host name is:-", host_Name )
